da5c9381be1369260d52eda115b223cb83b008d9
I300289 <smarika@sap.com>
2019-03-12 15:56:20 +0530

    functors

--- src/main/MonadsImpl.scala
+++ src/main/MonadsImpl.scala
@@ -2,12 +2,22 @@
 
 object MonoidImpl {
 
-  /* Just what is a monoid? It’s simply a type A and an implementation of Monoid[A] that satisfies the laws.
-  Stated tersely, a monoid is a type together with a binary operation (op) over that type, satisfying associativity
-  and having an identity element (zero).
+  /* The names functor and monad come from the branch of mathematics called category theory
+  * */
 
-  easy and basic implemetation of manning exercise for Monoids*/
+  trait Functor[F[_]] {
+    def map[A,B](fa: F[A])(f: A => B): F[B]
+  }
 
+
+
+  trait Monad[F[_]] extends Functor[F] {
+    def unit[A](a: => A): F[A]
+    def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B]
+    def map[A,B](ma: F[A])(f: A => B): F[B] = flatMap(ma)(a => unit(f(a)))
+    Since Monad provides a default implementation of map, it can extend Functor. All monads are functors, but not all functors are monads.
+    def map2[A,B,C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] = flatMap(ma)(a => map(mb)(b => f(a, b)))
+  }
 
 
 
