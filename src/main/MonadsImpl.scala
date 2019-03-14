16751c1d1d46df39e904285fd874c42c071fa220
I300289 <smarika@sap.com>
2019-03-14 11:21:24 +0530

    changes

--- src/main/MonadsImpl.scala
+++ src/main/MonadsImpl.scala
@@ -2,6 +2,7 @@
 
 object MonoidImpl {
 
+
   /* The names functor and monad come from the branch of mathematics called category theory
   * */
 
@@ -19,6 +20,29 @@ object MonoidImpl {
     def map2[A,B,C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] = flatMap(ma)(a => map(mb)(b => f(a, b)))
   }
 
+//simple
+
+  trait M[A] {
+    def flatMap[B](f: A => M[B]): M[B]
+  }
+
+  def unit[A](x: A): M[A] //outside trait body /* * “Unit” is merely a convention for referencing monad’s identity operation in Scala/
+
+  /**
+    *  identity (return in Haskell, unit in Scala)
+       bind (>>= in Haskell, flatMap in Scala) */
+
+  /* we want to wrap some object with our monad wrapper, we must parameterize the monad with
+  the type of the underlying object, e.g. M[Int], M[String], M[MyClass] etc*/
+
+
+  /* Let’s say U is a List. It works for various other types, but we’ll use List for this example. Now, what flatMap
+  does is that it takes a function with signature A → List[B] and it uses that function to transform the
+  underlying object of type A into a List[B]. This operation is known as map. Since we transformed our underlying A into a List[B],
+  this leaves us with a List[List[B]]. But we did not use ordinary map() — we used flatMap().
+   This means that the job is not done yet; flatMap will now “flatten” our List[List[B]] into List[B].
+    //link: https://medium.freecodecamp.org/demystifying-the-monad-in-scala-cc716bb6f534
+*/
 
 
 }
